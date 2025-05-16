d966712f76d023c0f52556763ec47bf091a7a483
Faridun Ismoilov <faridun.ismoilov@swfx.ch>
2025-05-16 18:39:58 +0500

    fix

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import './App.css'; // Опциональный CSS-файл
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <header>
+        <h1>
+          Hello World
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // Вы можете создать этот файл или удалить эту строку
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+); 
